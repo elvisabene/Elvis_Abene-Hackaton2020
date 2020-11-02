eafa440ee6d8832f4b6cc68f2378bc319740cb51
ElvisA <abene197@gmail.com>
2020-11-02 22:18:19 +0300

    Create Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = GetArrayLength();
+            int[] array = ArrayWithRandomNumbers(length);
+            PrintArrayWithMessage(array, "Исходный массив:");
+            PrintArrayWithMessage(BubbleSort(array), "Массив после сортировки:");
+            Console.ReadKey();
+        }
+        static int[] ArrayWithRandomNumbers(int length)
+        {
+            int[] array = new int[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+            return array;
+        }
+        static void PrintArrayWithMessage(int[] array, string message)
+        {
+            Console.WriteLine("\n" + message + "\n");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+        static int GetArrayLength()
+        {
+            Console.WriteLine("Введите размер массива:");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+            {
+                Console.WriteLine("Неверный символ! Повторите попытку! Возможно размер массива слишком мал.");
+            }
+            return number;
+        }
+        static int [] BubbleSort(int [] array)
+        {
+            for (int i = 0; i<array.Length-1; i++)
+            {
+                for (int j = i+1; j<array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
