fb7ce88c87147dfa2b3e247c6b765c6d91f640c3
ElvisA <abene197@gmail.com>
2020-11-04 10:44:57 +0300

    Edit#1 in Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,106 +2,154 @@
 
 namespace Task2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Для ввода массива с клавитуры нажмите                  \"1\"\n" +
                               "Для заполнения массива случайными числами нажмите      \"2\"");
 
-            int result = GetNumber1Or2();
+            int result = GetNumber("Number1or2", out int number, "Неверный символ! Повторите попытку! Введите символ 1 или 2!");
             Console.WriteLine("Введите размер массива:");
-            int length = GetArrayLength();
+            int length = GetNumber("Length", out number, "Неверный символ! Повторите попытку! Возможно размер массива слишком мал.");
             int[] array;
             if (result == 1)
             {
-                array = ArrayFromUser(length);
+                array = InputArray(length);
             }
             else
             {
-                array = ArrayWithRandomNumbers(length);
+                array = GetRandomArray(length);
             }
             string message = "Исходный массив:";
-            PrintArrayWithMessage(array,message);
-            Console.WriteLine($"\n{GetMessageAboutMostFrequentNumberIn(array)}.");
+            PrintArrayWithMessage(array, message);
+            Console.WriteLine(GetResult(array));
             Console.ReadKey();
         }
-        static int GetNumber1Or2()
-        {
-            int number;
-            while (!int.TryParse(Console.ReadLine(), out number) || number != 1 && number != 2)
-            {
-                Console.WriteLine("Неверный символ! Повторите попытку! Введите символ 1 или 2!");
-            }
-            return number;
-            
-        }
-        static int GetArrayLength()
-        {
-            int number;
-            while (!int.TryParse(Console.ReadLine(), out number) || number < 1)
-            {
-                Console.WriteLine("Неверный символ! Повторите попытку! Возможно размер массива слишком мал.");
-            }
-            return number;
-        }
-        static int[] ArrayFromUser(int length)
+
+        private static int[] InputArray(int length)
         {
             int[] array = new int[length];
-            for (int i = 0; i<length; i++)
+            for (int i = 0; i < length; i++)
             {
                 Console.WriteLine($"Введите {i + 1}-й элемент");
-                while (!int.TryParse(Console.ReadLine(), out array[i]))
-                {
-                    Console.WriteLine("Неверный формат! Повторите попытку!");
-                }
+                array[i] = GetNumber("", out int number, "Неверный формат! Повторите попытку!");
             }
             return array;
         }
-        static int[] ArrayWithRandomNumbers(int length)
+
+        private static int GetNumber(string subtype, out int number, string errorMessage)
+        {
+            while (true)
+            {
+                while (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                switch (subtype)
+                {
+                    case "Number1or2":
+                        if (number == 1 || number == 2)
+                        {
+                            return number;
+                        }
+                        else
+                        {
+                            Console.WriteLine(errorMessage);
+                            break;
+                        }
+                    case "Length":
+                        if (number > 0)
+                        {
+                            return number;
+                        }
+                        else
+                        {
+                            Console.WriteLine(errorMessage);
+                            break;
+                        }
+                    default:
+                        return number;
+                }
+            }
+        }
+
+        private static int[] GetRandomArray(int length)
         {
             int[] array = new int[length];
             Random random = new Random();
-            for (int i = 0; i<length; i++)
+            for (int i = 0; i < length; i++)
             {
                 array[i] = random.Next(-10, 10);
             }
             return array;
         }
-        static void PrintArrayWithMessage(int [] array, string message) 
+
+        private static void PrintArrayWithMessage(int[] array, string message)
         {
-            Console.WriteLine(message+"\n");
+            Console.WriteLine(message + "\n");
             foreach (int element in array)
             {
                 Console.Write(element + " ");
             }
         }
-        static string GetMessageAboutMostFrequentNumberIn(int [] array)
+
+        private static string GetResult(int[] array)
         {
-            int k = -1; //индекс наиболее встречаемого элемента
-            int f = 0;  //количество встреч элемента в массиве
-            for(int i = 0; i<array.Length; i++)
+            int[] count = new int[array.Length];
+            int index = 0;
+            array = SortArray(array);
+            for (int i = 0; i < array.Length; i++)
             {
-                int[] matches = Array.FindAll(array, x => x == array[i]);
-                if(matches.Length > f)
+                for (int j = i; j < array.Length; j++)
                 {
-                    if (matches.Length == 1)
+                    if (array[i] == array[j])
                     {
-                        continue;
+                        count[i]++;
                     }
-                    f = matches.Length;
-                    k = i;
+                }
+                if (i > 0)
+                {
+                    if (count[i - 1] < count[i])
+                    {
+                        index = i;
+                    }
                 }
             }
-            if (k == -1)
+            if (count[0] > count[index])
             {
-                return "В массиве все числа встречаются по одному разу!";
+                index = 0;
             }
-            else  
+            else if (count[count.Length - 1] > count[index])
             {
-                return $"Чаще всего в массиве встречается число {array[k].ToString()}";
+                index = count.Length - 1;
             }
-
+            if (index == 0 && count[0] == 1)
+            {
+                return "\nВ массиве все числа встречаются по одному разу";
+            }
+            else
+            {
+                return $"\nЧаще всего в массиве встречается число {array[index].ToString()}.";
+            }
         }
+
+        private static int[] SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
     }
 }
