cbd3a738dfb6a42126872601ae8b0f4ab112bb42
ElvisA <abene197@gmail.com>
2020-11-05 22:54:40 +0300

    Coomit1

---
+++ Remove duplicate words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Remove_duplicate_words
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
