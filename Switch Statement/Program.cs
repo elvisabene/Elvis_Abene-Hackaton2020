c535e933e8ab5aae3213950a88599be665bda295
ElvisA <abene197@gmail.com>
2020-11-05 22:46:36 +0300

    Create Switch-statement

--- Switch Statement/Program.cs
+++ Switch Statement/Program.cs
@@ -6,12 +6,47 @@ namespace Switch_Statement
     {
         private static void Main(string[] args)
         {
-            
+            object ob = new object();
+            Console.WriteLine(GetType(ob));
             Console.ReadKey();
         }
         private static string GetType(object arg)
         {
-            
+            const string String = "String";
+            const string Char = "Char";
+            const string Int = "Int32";
+            const string Double = "Double";
+            const string Byte = "Byte";
+            const string Decimal = "Decimal";
+            const string Bool = "Boolean";
+            const string Float = "Single";
+            const string Long = "Int64";
+            const string Short = "Int16";
+            switch (arg.GetType().Name)
+            {
+                case String:
+                    return $"{arg} is string.";
+                case Char:
+                    return $"{arg} is char.";
+                case Int:
+                    return $"{arg} is int.";
+                case Double:
+                    return $"{arg} is double.";
+                case Byte:
+                    return $"{arg} is byte.";
+                case Decimal:
+                    return $"{arg} is decimal.";
+                case Bool:
+                    return $"{arg} is bool.";
+                case Float:
+                    return $"{arg} is float.";
+                case Long:
+                    return $"{arg} is long.";
+                case Short:
+                    return $"{arg} is short.";
+                default:
+                    return $"{arg} is unknown type.";
+            }
         }
     }
 }
