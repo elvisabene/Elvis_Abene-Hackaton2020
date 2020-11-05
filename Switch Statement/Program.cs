30ca87ff22fc02e20b67be6b68d68fb7ca012b90
ElvisA <abene197@gmail.com>
2020-11-05 16:31:37 +0300

    Commit1

---
+++ Switch Statement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Switch_Statement
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+        private static string GetType(object arg)
+        {
+            
+        }
+    }
+}
