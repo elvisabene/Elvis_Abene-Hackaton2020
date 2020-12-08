b6cfcb8829f35b5e4db667727044757fea9bb584
ElvisA <abene197@gmail.com>
2020-12-08 19:00:33 +0300

    New Edit#2

--- Princess/Field.cs
+++ Princess/Field.cs
@@ -9,24 +9,32 @@ namespace Princess
         private const char empty = ' ';
         private const char notBomb = '0';
         private const char wall = '#';
+        private int setBombCase = new Random().Next(1, 9);
+        private char[,] field = new char[12, 23];
+        private char[] bombCollection = new char[10];
 
         public Field()
         {
-            char[] bombCollection = new char[10];
-            for(int i = 0; i < 10; i++)
+            GetBombs();
+            GenerateField();
+        }
+        private void GetBombs()
+        {
+            for (int i = 0; i < 10; i++)
             {
                 bombCollection[i] = bomb;
             }
+        }
+        private void GenerateField()
+        {
             Random randomSetBomb = new Random();
-            int setBombCase = 1;
-            char [,] field = new char[12, 23];
             for (int i = 0; i < field.GetLength(0); i++)
             {
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
                     bool notStartOrEnd = (i != 1 && j != 2) || (i != 10 && j != 20);
                     bool hasBombs = Array.Exists(bombCollection, x => x == bomb);
-                    if ( i == 0 || j == 0 || i == 11 || j == 22)
+                    if (i == 0 || j == 0 || i == 11 || j == 22)
                     {
                         if (j % 2 == 0)
                         {
@@ -39,7 +47,7 @@ namespace Princess
                     }
                     else
                     {
-                        if (notStartOrEnd && randomSetBomb.Next(1,9) == setBombCase && j % 2 == 0 && hasBombs)
+                        if (notStartOrEnd && randomSetBomb.Next(1, 9) == setBombCase && j % 2 == 0 && hasBombs)
                         {
                             Console.Write(bomb);
                             IsBomb[i, j] = true;
