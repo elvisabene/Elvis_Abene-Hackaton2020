b390b3a35e9448fa46bd2cd7dce1e072a04c080b
ElvisA <abene197@gmail.com>
2020-12-29 23:40:48 +0300

    Create Bomb

---
+++ Bomb/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Game.NewGameRequested)
+            {
+                Game.Start();
+            }
+        }
+    }
+}
